d5ccfd2dc664859ea4b2a6615658e007f2f840ab
Jerry Cami Jean Baptiste <jerry.cami.jeanbaptiste@gmail.com>
2025-12-14 14:54:10 -0500

    d

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,10 @@
-
-// src/lib/config.ts
-import Medusa from "@medusajs/js-sdk";
+import Medusa from "@medusajs/js-sdk"
 
 export const sdk = new Medusa({
   baseUrl: "https://jbssupply.medusajs.app",
   debug: process.env.NODE_ENV === "development",
   publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
-  auth: {
-    type: "session",  // Tell the SDK you're using session auth added+
-    fetchCredentials: "include",  // This sends cookies automatically added+
-  },
-});
+
+
+
+})
